f832c15f24b9bbd344f01722636ad71ab6c84c3d
Chen.Tao <chen.tao@dynatacorp.com>
2021-03-15 14:10:31 +0800

    Initial Commit

---
+++ shared/page-header/page-header.js
@@ -0,0 +1,23 @@
+// shared/page-header/page-header.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
