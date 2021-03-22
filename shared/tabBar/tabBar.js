b7082a9905a849148a648e96d138a521819f39c1
Chen.Tao <chen.tao@dynatacorp.com>
2021-03-22 18:09:54 +0800

    addthehomepageselfinfosection
    selfinfo add photo circle user id, points part

--- shared/tabBar/tabBar.js
+++ shared/tabBar/tabBar.js
@@ -2,23 +2,36 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-    tabBar: [
-      {
+    tabBar: [{
         "pagePath": "/pages/authroize/authroize",
-        "text": "首页",
-        "iconPath": "../../images/index_icon.png" // 因为子页面点击图标的不需要变化，因为直接跳转到首页了
+        "text": "Home",
+        "iconPath": "../../img/wxlogo.png" // 因为子页面点击图标的不需要变化，因为直接跳转到首页了
       },
       {
         "pagePath": "/pages/joinPrePage/joinPrePage",
-        "text": "Join",
-        "iconPath": "../../images/user_icon.png"
+        "text": "Profile",
+        "iconPath": "../../img/wxlogo.png"
+      },
+      {
+        "pagePath": "/pages/joinPrePage/joinPrePage",
+        "text": "Survey",
+        "iconPath": "../../img/wxlogo.png"
+      },
+      {
+        "pagePath": "/pages/joinPrePage/joinPrePage",
+        "text": "Cash Out",
+        "iconPath": "../../img/wxlogo.png"
+      },
+      {
+        "pagePath": "/pages/joinPrePage/joinPrePage",
+        "text": "FAQ",
+        "iconPath": "../../img/wxlogo.png"
       }
     ]
   },
@@ -29,7 +42,7 @@ Component({
   methods: {
     navigateDetail: function (e) {
       wx.reLaunch({ // 关闭所有打开过的页面，跳转到相对于的页面
-        url: e.currentTarget.dataset.url  // 获取tabbar.wxml中data-url传递的参数
+        url: e.currentTarget.dataset.url // 获取tabbar.wxml中data-url传递的参数
       })
     }
   }
