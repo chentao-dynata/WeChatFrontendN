3ffd63910b245137d7ce4549e6f8229035e70a60
Chen.Tao <chen.tao@dynatacorp.com>
2021-03-25 13:58:07 +0800

    add the pages
    add some pages in the tab bar

---
+++ shared/active-suvey-item/active-survey-item.js
@@ -0,0 +1,23 @@
+// shared/active-suvey-item/active-survey-item.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
