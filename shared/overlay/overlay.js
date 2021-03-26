12618b09f959d99fcc2296561566951d14c6d450
Chen.Tao <chen.tao@dynatacorp.com>
2021-03-26 12:01:27 +0800

    add an overlay component
    use overlay to cover page details when busy

---
+++ shared/overlay/overlay.js
@@ -0,0 +1,26 @@
+// shared/overlay/overlay.js
+Component({
+  /**
+   * Component properties
+   */
+  properties: {
+    show:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+
+  }
+})
