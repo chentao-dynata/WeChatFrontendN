9d077edc915fd260d55ecd61a1cf4e0f97088f54
Chen.Tao <chen.tao@dynatacorp.com>
2021-04-08 14:08:33 +0800

    add some cash out page code
    put script calls in js file for now

--- pages/postPageCashout/postPageCashout.js
+++ pages/postPageCashout/postPageCashout.js
@@ -1,18 +1,43 @@
 // pages/postPageCashout/postPageCashout.js
+
+const {
+  selectTopic
+} = require("../../utils/translation_en");
+
+const app = getApp();
+const translation = app.translation;
+
+
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    translation,
+    points: app.service.get('points') || 0,
+    amount: 0,
+    min: 0,
+    max: 0,
+    cashOutFlow: 1,
+    select: false,
+    dropdownPlaceholder: 'Select Amount',
+    reward: null,
+    isAmountSelected: false,
+    firstName: null,
+    lastName: null,
+    alipayId: null,
+    isAlipayValid: false,
+    readyToSubmit: false
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    this.memberInfo();
+    this.getRewards();
+    console.log('alipay valid: ', this.data.isAlipayValid)
   },
 
   /**
@@ -61,6 +86,71 @@ Page({
    * Called when user click on the top right corner to share
    */
   onShareAppMessage: function () {
+
+  },
+  memberInfo(e) {
+    var self = this;
+    wx.request({
+      url: app.config.balanceUrl + '?session=' + app.service.get('novaSession'),
+      method: 'GET',
+      complete(res) {
+        console.log('Claim response: ', res);
+
+        if (res.statusCode && res.statusCode == 401) {
+          console.log('token expired!!')
+          // token expired, log out user
+          app.service.clearStore();
+          wx.redirectTo({
+            // url: '../auth/auth',
+            url: '../login/login',
+          })
+        } else if (res.data && res.data.response) {
+          self.setData({
+            points: parseFloat(res.data.response.amount)
+          });
+          if (res.data.response.amount !== app.service.points) {
+            console.log('points have been updated, updating main service data value....')
+            // app.service.setData({
+            //   points: res.data.response.amount
+            // })
+            app.service.set('points', res.data.response.amount);
+          }
+        }
+
+      }
+    });
+  },
+  getRewards() {
+    var self = this;
+    wx.request({
+      url: app.config.getRewardsUrl + '?session=' + app.service.get('novaSession'),
+      method: 'GET',
+      complete(res) {
+        console.log('Rewards response: ', res);
+
+        if (res.statusCode && res.statusCode == 401) {
+          console.log('token expired!!')
+          app.service.clearStore();
+          wx.redirectTo({
+            // url: '../auth/auth',
+            url: '../login/login',
+          })
+        }
+        if (!res.data) return
+        if (res.data.response) {
+          for (let i = 0; i < res.data.response.length; i++) {
+            // console.log(i + ': ' + res.data.response[i].reward.rewardName + ' ' + res.data.response[i].reward.rewardName)
+            if (res.data.response[i].reward.id == 7151) {
+              console.log('found one', res.data.response[i])
+              self.setData({
+                reward: res.data.response[i].reward
+              })
+
+            }
+          }
+        }
+      }
+    });
 
   }
 })
